8f9b0c0d86c6a42004cc12bed5d89e66b3d38e32
Andrew Bainbridge <2005@deadfrog.co.uk>
2023-10-17 12:01:44 +0100

    Move GUI code into Deadfrog Lib. Lots of things were renamed to match the Deadfrog Lib naming conventions.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 // Deadfrog lib headers
 #include "df_clipboard.h"
 #include "df_font.h"
+#include "df_gui.h"
 #include "df_time.h"
 #include "df_window.h"
 
 // Project headers
 #include "dict.h"
-#include "gui.h"
 
 // Standard headers
 #include <ctype.h>
@@ -18,11 +18,11 @@ static const char APPLICATION_NAME[] = "21st Century Dictionary";
 
 static DfWindow *g_win;
 static dict_t g_dict;
-static edit_box_t g_edit_box = { 0 };
-static button_t g_help_button = { "Help" };
-static list_view_t g_list_view = { 0 };
+static DfEditBox g_edit_box = { 0 };
+static DfButton g_help_button = { "Help" };
+static DfListView g_list_view = { 0 };
 static char const *g_current_word = NULL;
-static text_view_t g_text_view = { 0 };
+static DfTextView g_text_view = { 0 };
 
 // Returns 1 on match, 0 otherwise.
 static int wildcard_match(char const *haystack, char const *needle) {
@@ -54,21 +54,21 @@ static int wildcard_match(char const *haystack, char const *needle) {
 }
 
 
-static void populate_list_view(list_view_t *lv, dict_t *dict, char const *filter) {
-    lv->num_items = 0;
+static void populate_list_view(DfListView *lv, dict_t *dict, char const *filter) {
+    lv->numItems = 0;
 
     unsigned filter_len = strlen(filter);
     for (unsigned i = 0; i < dict->num_words; i++) {
         char const *word = dict->words[i];
         if (wildcard_match(word, filter)) {
-            lv->items[lv->num_items] = word;
-            lv->num_items++;
+            lv->items[lv->numItems] = word;
+            lv->numItems++;
         }
     }
 }
 
 static void draw_frame() {
-    gui_do_frame(g_win);
+    DfGuiDoFrame(g_win);
 
     int spacer = 7 * g_drawScale;
     int edit_box_y = spacer;
@@ -91,37 +91,37 @@ static void draw_frame() {
     RectFill(g_win->bmp, g_win->bmp->width - spacer, 0, spacer, g_win->bmp->height, g_frameColour);
     RectFill(g_win->bmp, text_view_x - spacer, 0, spacer, g_win->bmp->height, g_frameColour);
 
-    if (edit_box_do(g_win, &g_edit_box, spacer, edit_box_y, list_view_width, edit_box_height))
+    if (DfEditBoxDo(g_win, &g_edit_box, spacer, edit_box_y, list_view_width, edit_box_height))
         populate_list_view(&g_list_view, &g_dict, g_edit_box.text);
 
-    if (button_do(g_win, &g_help_button, help_button_x, help_button_y,
+    if (DfButtonDo(g_win, &g_help_button, help_button_x, help_button_y,
         help_button_w, help_button_h)) {
         strcpy(g_edit_box.text, "help text");
         populate_list_view(&g_list_view, &g_dict, g_edit_box.text);
     }
 
-    list_view_do(g_win, &g_list_view, spacer, views_y, list_view_width, views_height);
+    DfListViewDo(g_win, &g_list_view, spacer, views_y, list_view_width, views_height);
     char const *selected_word = "";
-    if (g_list_view.selected_item >= 0)
-        selected_word = g_list_view.items[g_list_view.selected_item];
+    if (g_list_view.selectedItem >= 0)
+        selected_word = g_list_view.items[g_list_view.selectedItem];
     if (selected_word != g_current_word) {
-        text_view_empty(&g_text_view);
+        DfTextViewEmpty(&g_text_view);
 
         int def_indices[6];
         int num_defs = dict_get_def_indices(&g_dict, selected_word, def_indices);
         for (int i = 0; i < num_defs; i++) {
-            text_view_add_text(&g_text_view, dict_get_clean_def_text(&g_dict, def_indices[i]));
-            text_view_add_text(&g_text_view, "\n\n");
+            DfTextViewAddText(&g_text_view, dict_get_clean_def_text(&g_dict, def_indices[i]));
+            DfTextViewAddText(&g_text_view, "\n\n");
         }
 
         g_current_word = selected_word;
     }
 
-    text_view_do(g_win, &g_text_view, text_view_x, views_y, text_view_width, views_height);
+    DfTextViewDo(g_win, &g_text_view, text_view_x, views_y, text_view_width, views_height);
 
     if (g_win->input.keyDowns[KEY_C] && g_win->input.keys[KEY_CONTROL]) {
         int num_chars;
-        char const *selected_text = text_view_get_selected_text(&g_text_view, &num_chars);
+        char const *selected_text = DfTextViewGetSelectedText(&g_text_view, &num_chars);
         if (selected_text) {
             ClipboardSetData(selected_text, num_chars);
         }
